4956c947bc40dd3c562f620c97c396e80b5bfc9f
aleosse601-stack <aleosse601@gmail.com>
2026-04-27 09:28:22 +0200

    Disable stale PWA cache

--- sw.js
+++ sw.js
@@ -1,16 +1,14 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('falla-app').then(cache => cache.addAll([
-      './',
-      './login.html',
-      './admin.html',
-      './fallero.html'
-    ]))
-  );
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
